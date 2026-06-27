2373de35d4a1ef50485f224c9c5e38a265885fde
Vivek Ranjan <ranjanvivek498@gmail.comm>
2026-06-27 20:45:39 +0530

    js start

---
+++ web/Javascript/basics01/script.js
@@ -0,0 +1,22 @@
+console.log("Hello world!");
+
+// variables  -> let , const , var
+
+let name = "Dikshya Software Engg and also AI/ML Engg";
+console.log(name);
+// name = "vivek";
+// console.log(name);
+
+const country = "Nepal";
+console.log(country);
+ country="India";
+console.log(country);
+
+let age = 19;
+console.log(age);
+
+let crc = true;
+console.log(crc)
+
+console.log(name);
+
