40b24510502297b27262d3d6c5abde0ed0420303
Vivek Ranjan <ranjanvivek498@gmail.comm>
2026-07-05 22:52:35 +0530

    JavaScript

---
+++ control-flow/script.js
@@ -0,0 +1,38 @@
+// if statement
+// let age = 18; 
+// if (age >= 18){
+//     console.log("you can vote");
+// }
+
+// if else
+// let age = 22; 
+// if (age >= 18){
+//     console.log("you can vote");
+// }
+// else{
+//     console.log("you cannot vote");
+// }
+
+// SWITCH-STATEMENT
+// let day = 3;
+// switch (day){
+//     case 1: console.log("Monday");
+//     break;
+
+//     case 2: console.log("Tues");
+//     break;
+
+//     case 3: console.log("Wedn");
+//     break;
+
+//     case 4: console.log("Thrus");
+//     break;
+
+//     default:
+//         console.log("Invalid day");
+// }
+
+const prompt = require("prompt-sync")();
+let name = prompt("Enter your name:");
+
+console.log(`My name is ${name}`);
--- web/Javascript/basics01/script.js
+++ web/Javascript/basics01/script.js
@@ -2,21 +2,99 @@ console.log("Hello world!");
 
 // variables  -> let , const , var
 
-let name = "Dikshya Software Engg and also AI/ML Engg";
-console.log(name);
+// let name = "Dikshya Software Engg and also AI/ML Engg";
+// console.log(name);
 // name = "vivek";
 // console.log(name);
 
-const country = "Nepal";
-console.log(country);
- country="India";
-console.log(country);
+// const country = "Nepal";
+// console.log(country);
+//  country="India";
+// console.log(country);
 
-let age = 19;
-console.log(age);
+// let age = 19;
+// console.log(age);
 
-let crc = true;
-console.log(crc)
+// let crc = true;
+// console.log(crc)
 
-console.log(name);
+// console.log(name);
+
+
+//OPERATORS
+//Airthmetic opr
+//Addition
+let a = 5;
+let b = 5;
+let c = a+b;
+console.log(c);
+
+//subtraction
+let d = 19;
+let e = 11;
+let f = d-e;
+console.log(f);
+
+//mul
+let m1 = 24;
+let m2 = 3;
+// let m3 = m1*m2
+console.log(m1*m2);
+
+//Divide
+let d1 = 81;
+let d2 = 9;
+let d3 = d1/d2;
+console.log(d3);
+
+//Assignment Opr
+// += (Add and Assign)
+// let x = 10;
+// x += 5;   // x = x + 5
+// console.log(x); // 15
+
+// -= (Subtract and Assign)
+// let x = 10;
+// x -= 3;   // x = x - 3
+// console.log(x); // 7
+
+// // *= (Multiply and Assign)
+// let x = 10;
+// x *= 2;   // x = x * 2
+// console.log(x); // 20
+
+// /= (Divide and Assign)
+// let x = 20;
+// x /= 4;   // x = x / 4
+// console.log(x); // 5
+
+// %= (Modulus and Assign)
+// let x = 10;
+// x %= 3;   // x = x % 3
+// console.log(x); // 1
+
+// ** power  **= (Exponent and Assign)
+// **= को Exponent Assignment Operator कहते हैं।
+// let x = 2;
+// x **= 3;
+// console.log(x);
+
+// Step by Step
+// Step 1
+// let x = 2;
+// अब x = 2
+// Step 2
+// x **= 3;
+// यह internally ऐसे चलता है:
+// x = x ** 3;
+// यानि
+// x = 2 ** 3;
+// अब 2³ (2 × 2 × 2) = 8
+// तो अब
+// x = 8;
+// Step 3
+// console.log(x);
+// Output:
+// 8
+
 
