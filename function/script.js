98304364cf8e92185847b6d3ee724d63e235ef11
Vivek Ranjan <ranjanvivek498@gmail.comm>
2026-07-18 23:14:13 +0530

    js practice

---
+++ function/script.js
@@ -0,0 +1,31 @@
+// function greet(){
+//     console.log("Hello this is my 1st function");
+// }
+
+// greet();
+
+// Variable fn
+// const namaste = function(){
+//     console.log("Hello");
+// }
+
+// namaste();
+
+
+// Arrow function
+//  const greet = () => {
+//     console.log("Helloo")
+//  }
+
+//  greet();
+
+
+// one-line Arrow-fn
+// const greet = () => console.log("Helo");
+// greet();
+
+
+// let arr=[10,20,30,40,50];
+// for(let i = 0; i<arr.length; i++){
+//     console.log(arr[i]);
+// }
---
+++ loop/script.js
@@ -0,0 +1,22 @@
+// for(let i = 1;i>=5; i++){
+//     console.log(i)
+// }
+
+// let i = 1;
+// while (i <= 7) {
+//     console.log(i);
+//     i++;
+// }
+
+// let i = 1;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i <= 4);
+
+let i = 10;
+do {
+    console.log(i);
+} 
+while (i < 5);
+
