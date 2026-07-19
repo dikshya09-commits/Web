5b62385aba8caa8cf3718085cc84bee8156db3e7
Vivek Ranjan <ranjanvivek498@gmail.comm>
2026-07-20 01:26:15 +0530

    JS-Practice-Question

---
+++ PracticeJS/script.js
@@ -0,0 +1,39 @@
+// Practice Question - Conditional statement
+// (Ques 1) Do numbers me bada number print karo.
+// let a = 10;
+// let b = 20;
+
+// if(a>b){
+//     console.log(a);
+// }else{
+//     console.log(b);
+// }
+
+// (Ques 2) Teen numbers me sabse bada number print karo.
+// let a = 10;
+// let b = 20;
+// let c = 30;
+
+// if(a>b && a>c){
+//     console.log(a);
+// }else if(b>a && b>c){
+//     console.log(b);
+// }else{
+//     console.log(c);
+// }
+
+// (Ques 3) Check karo number 10 ke equal hai ya nahi.
+// let a = 10;
+// if(a == 10){
+//     console.log("This no is equal to 10")
+// }else{
+//     console.log("This no is not equal to 10")
+// }
+
+// (Ques 4) Check karo year leap year hai ya nahi.
+let year = 2024
+if( year % 4 == 0){
+    console.log("leap year");
+}else{
+    console.log("not leap year");
+}
